38677825dbe9adeefc14d5562616fbaec7b6cc48
RidaKo <ridas.kozukauskas@gmail.com>
2024-10-01 12:10:33 +0300

    Switched templates to Blazor Web from MVC

--- lithuanian-language-learning-tool.Web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace lithuanian_language_learning_tool.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
